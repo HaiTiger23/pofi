9da8854269f5b933fb82ea861b2573014818b726
Hai dev <haitiger.al@gmail.com>
2025-04-04 17:06:06 +0700

    Remove song history toggle from sidebar menu

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -126,18 +126,7 @@
             </label>
           </div>
         </li>
-        <li>
-          <div class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-            <label class="relative inline-flex items-center cursor-pointer">
-              <input type="checkbox" v-model="showHistory" @change="toggleHistory" class="sr-only peer">
-              <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-              <span class="flex-1 ml-3 text-left whitespace-nowrap">
-                <i class="ri-history-line mr-2"></i>
-                Lịch sử bài hát
-              </span>
-            </label>
-          </div>
-        </li>
+       
       </ul>
     </div>
   </div>
